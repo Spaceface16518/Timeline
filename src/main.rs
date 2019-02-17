6d1c7706818cacbf6d4a7ade9834f849244659c4
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2019-02-16 20:39:12 -0600

    fmt

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,7 @@ use timeline::Entry;
 fn main() {
     let input = App::from_args();
     match input.subcmd {
-        Command::Parse { input
-        } => parse(input),
+        Command::Parse { input } => parse(input),
     };
 }
 
@@ -28,47 +27,43 @@ enum Command {
     )]
     Parse {
         #[structopt(flatten)]
-        input: Parse
+        input: Parse,
     },
 }
 
 #[derive(Debug, StructOpt)]
 struct Parse {
-#[structopt(
-            short = "y",
-            long = "yaml",
-            help = "Outputs in YAML instead of JSON"
-        )]
-        yaml: bool,
-        #[structopt(
-            short = "p",
-            long = "pretty",
-            help = "Pretty prints outputs"
-        )]
-        pretty: bool,
-        #[structopt(
-            short = "l",
-            long = "label",
-            help = "The label for this entry"
-        )]
-        label: String,
+    #[structopt(
+        short = "y",
+        long = "yaml",
+        help = "Outputs in YAML instead of JSON"
+    )]
+    yaml: bool,
+    #[structopt(short = "p", long = "pretty", help = "Pretty prints outputs")]
+    pretty: bool,
+    #[structopt(
+        short = "l",
+        long = "label",
+        help = "The label for this entry"
+    )]
+    label: String,
 
-        #[structopt(
-            short = "t",
-            long = "tag",
-            help = "An optional tag for the entry"
-        )]
-        tag: Option<String>,
+    #[structopt(
+        short = "t",
+        long = "tag",
+        help = "An optional tag for the entry"
+    )]
+    tag: Option<String>,
 
-        #[structopt(
-            short = "s",
-            long = "start",
-            help = "The start year or point year"
-        )]
-        start: i32,
+    #[structopt(
+        short = "s",
+        long = "start",
+        help = "The start year or point year"
+    )]
+    start: i32,
 
-        #[structopt(short = "e", long = "end", help = "The end year")]
-        end: i32,
+    #[structopt(short = "e", long = "end", help = "The end year")]
+    end: i32,
 }
 
 fn parse(parse: Parse) {
