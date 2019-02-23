694c0d66e9a7428cf908f828a70ed8766a3d57ad
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2019-02-22 23:32:25 -0600

    Use more constant elements
    Using named constants and const fn's should improve performance, as well as clarifying the purpose of arbitrary string literals.
    Also use the Entry API in the text handler.

--- src/frontend/render/text.rs
+++ src/frontend/render/text.rs
@@ -5,7 +5,27 @@ use rayon::iter::{
 };
 use std::{cell::RefCell, collections::BinaryHeap};
 use timeline::Entry;
-use vec_map::VecMap;
+use vec_map::{
+    Entry::{Occupied, Vacant},
+    VecMap,
+};
+
+const INDENT: &str = " ";
+const PRELUDE: &str = "| ";
+const EMPTY_LINE: &str = "|\n";
+const SAME_LINE_INDENT: &str = "\\";
+const SAME_LINE_PRELUDE: &str = " \\";
+const NEWLINE: &str = "\n";
+const INDENT_SIZE: usize = 1;
+const PRELUDE_SIZE: usize = 2;
+
+const fn interval(start: i32, end: i32, len: usize) -> usize {
+    (end - start) as usize / (len * 3 / 2)
+}
+
+const fn index(start: i32, end: i32, interval: usize) -> usize {
+    (end - start) as usize / interval
+}
 
 pub fn text_handler(mut entries: Vec<Entry>) -> String {
     entries.sort_unstable();
@@ -24,19 +44,18 @@ fn indices(
 ) -> VecMap<RefCell<BinaryHeap<Entry>>> {
     let mut indices = VecMap::<RefCell<BinaryHeap<Entry>>>::new();
     for entry in entries {
-        let i = (entry.start() - start) as usize / interval;
-        if let Some(v) = indices.get_mut(i) {
-            let v = v.get_mut();
-            v.push(entry);
-        } else {
-            indices.insert(
-                i,
-                RefCell::new({
+        let i = index(start, entry.start(), interval);
+        match indices.entry(i) {
+            Occupied(mut e) => {
+                e.get_mut().get_mut().push(entry);
+            },
+            Vacant(e) => {
+                e.insert(RefCell::new({
                     let mut tmp = BinaryHeap::with_capacity(1);
                     tmp.push(entry);
                     tmp
-                }),
-            );
+                }));
+            },
         }
     }
     indices
@@ -50,8 +69,8 @@ fn outputs(
 ) -> String {
     let mut outputs = String::new();
     for i in (start..=end).step_by(interval) {
-        if let Some(v) = indices.remove((i - start) as usize / interval) {
-            outputs += "| ";
+        if let Some(v) = indices.remove(index(start, i, interval)) {
+            outputs += PRELUDE;
             let line: String = v
                 .into_inner()
                 .into_par_iter()
@@ -59,33 +78,48 @@ fn outputs(
                 .enumerate()
                 .map(|(indent, e)| {
                     let s = e.to_string();
-                    let mut pre = String::with_capacity(s.len() + indent + 2);
+                    let mut pre =
+                        String::with_capacity(s.len() + indent + PRELUDE_SIZE);
                     if indent > 0 {
-                        pre.push_str(" \\");
+                        pre += SAME_LINE_PRELUDE;
                         let mut curr_indent_pos = 0;
                         while curr_indent_pos < indent {
-                            pre.push('\\');
-                            curr_indent_pos += 1;
+                            pre += SAME_LINE_INDENT;
+                            curr_indent_pos += INDENT_SIZE;
                         }
                     }
-                    pre.push(' ');
-                    pre + &s + "\n"
+                    pre += INDENT;
+                    pre + &s + NEWLINE
                 })
                 .collect();
             outputs += &line;
         } else {
-            outputs += "|\n";
+            outputs += EMPTY_LINE;
         }
-        outputs += "\n";
     }
     outputs
 }
 
-const fn interval(start: i32, end: i32, len: usize) -> usize {
-    // (end - start) as usize / (len * 3 / 2)
-    index(start, end, len * 3 / 2)
-}
+#[cfg(test)]
+mod const_tests {
+    use super::{
+        INDENT,
+        INDENT_SIZE,
+        PRELUDE,
+        PRELUDE_SIZE,
+        SAME_LINE_INDENT,
+        SAME_LINE_PRELUDE,
+    };
 
-const fn index(start: i32, end: i32, interval: usize) -> usize {
-    (end - start) as usize / interval
+    #[test]
+    fn assert_indent_size() {
+        assert_eq!(INDENT.len(), INDENT_SIZE);
+        assert_eq!(SAME_LINE_INDENT.len(), INDENT_SIZE);
+    }
+
+    #[test]
+    fn assert_prelude_size() {
+        assert_eq!(PRELUDE.len(), PRELUDE_SIZE);
+        assert_eq!(SAME_LINE_PRELUDE.len(), PRELUDE_SIZE);
+    }
 }
